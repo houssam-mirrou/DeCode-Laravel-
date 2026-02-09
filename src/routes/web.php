3a583d8935e6431ea9c8c839f4cd48b0e78eebe8
houssam-mirrou <hossam.mirrou@gmail.com>
2026-02-09 09:05:31 +0100

    This commit i fixed the middleware in it

--- src/routes/web.php
+++ src/routes/web.php
@@ -22,26 +22,26 @@ Route::get('/login', [AuthController::class, 'log_in'])->name('login')->middlewa
 Route::post('/submit_login', [AuthController::class, 'submit_login'], 'submit_login');
 Route::post('/logout', [AuthController::class, 'log_out'])->name('logout')->middleware("auth");
 
-Route::get('/admin/dashboard', [AdminDashBoardController::class, 'index'])->name('admin_dashboard')->middleware("admin");
+Route::get('/admin/dashboard', [AdminDashBoardController::class, 'index'])->name('admin_dashboard')->middleware(["auth", "admin"]);
 
-Route::post('/admin/classes/assign-teachers', [ClassesController::class, 'assign_teacher'])->name('classes.assign_teacher')->middleware("admin");
-Route::delete('/admin/classes/remove-teacher', [ClassesController::class, 'remove_teacher'])->name('classes.remove_teacher')->middleware("admin");
-Route::resource('/admin/classes', ClassesController::class)->middleware("admin")->except(['show', 'create','edit']);
+Route::post('/admin/classes/assign-teachers', [ClassesController::class, 'assign_teacher'])->name('classes.assign_teacher')->middleware(["auth", "admin"]);
+Route::delete('/admin/classes/remove-teacher', [ClassesController::class, 'remove_teacher'])->name('classes.remove_teacher')->middleware(["auth", "admin"]);
+Route::resource('/admin/classes', ClassesController::class)->middleware(["auth", "admin"])->except(['show', 'create','edit']);
 
 
-Route::resource('/admin/competences', CompetencesController::class)->middleware("admin")->except(['create','show','edit']);
-Route::resource('/admin/sprints', SprintController::class)->middleware("admin")->except(['create','show','edit']);
-Route::resource('/admin/users', UsersController::class)->middleware("admin")->except(['create','show','edit']);
+Route::resource('/admin/competences', CompetencesController::class)->middleware(["auth", "admin"])->except(['create','show','edit']);
+Route::resource('/admin/sprints', SprintController::class)->middleware(["auth", "admin"])->except(['create','show','edit']);
+Route::resource('/admin/users', UsersController::class)->middleware(["auth", "admin"])->except(['create','show','edit']);
 
-Route::resource('/teacher/briefs', BriefController::class)->middleware("teacher");
+Route::resource('/teacher/briefs', BriefController::class)->middleware(["auth","teacher"]);
 
-Route::get('/teacher/dashboard', [TeacherDashboardController::class, 'index'])->name('teacher_dashboard')->middleware("teacher");
-Route::get('//student/dashboard', [StudentDashBoardController::class, 'index'])->name('student_dashboard')->middleware('student');
-Route::post('/student/briefs/submit', [StudentDashBoardController::class, 'submit_brief'])->name('submit_brief')->middleware('student');
+Route::get('/teacher/dashboard', [TeacherDashboardController::class, 'index'])->name('teacher_dashboard')->middleware(["auth","teacher"]);
+Route::get('//student/dashboard', [StudentDashBoardController::class, 'index'])->name('student_dashboard')->middleware(["auth","student"]);
+Route::post('/student/briefs/submit', [StudentDashBoardController::class, 'submit_brief'])->name('submit_brief')->middleware(["auth","student"]);
 
-Route::resource('/student/briefs', ProjectController::class)->middleware('student')->names('project');
-Route::resource('/teacher/evaluations', EvaluationController::class)->middleware('teacher')->names('evaluation');
+Route::resource('/student/briefs', ProjectController::class)->middleware(["auth","student"])->names('project');
+Route::resource('/teacher/evaluations', EvaluationController::class)->middleware(["auth","teacher"])->names('evaluation');
 Route::get('/teacher/evaluate/{brief}/{student}', [EvaluationController::class, 'create'])
-    ->name('evaluation.create_custom');
-Route::get('/student/evaluations',[StudentEvalController::class,'index'])->name('student_eval')->middleware('student');
-Route::get('/teacher/students', [TeacherStudentsController::class , 'index'])->name('teacher_students')->middleware('teacher');
+    ->name('evaluation.create_custom')->middleware(["auth","teacher"]);
+Route::get('/student/evaluations',[StudentEvalController::class,'index'])->name('student_eval')->middleware(["auth","student"]);
+Route::get('/teacher/students', [TeacherStudentsController::class , 'index'])->name('teacher_students')->middleware(["auth","teacher"]);
